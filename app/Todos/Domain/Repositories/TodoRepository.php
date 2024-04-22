de62761f6cffb73777a9678a3f79f6ba03625329
Ali Makkawi <66633670+AliMakkawi@users.noreply.github.com>
2024-04-22 23:38:06 +0300

    Add method to get all todos

--- app/Todos/Domain/Repositories/TodoRepository.php
+++ app/Todos/Domain/Repositories/TodoRepository.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 namespace App\Todos\Domain\Repositories;
+use Illuminate\Support\Collection;
+
 interface TodoRepository
 {
+    public function getAllTodos(): Collection;
 }
--- app/Todos/Infrastructure/Repositories/EloquentTodoRepository.php
+++ app/Todos/Infrastructure/Repositories/EloquentTodoRepository.php
@@ -3,7 +3,13 @@
 declare(strict_types=1);
 
 namespace App\Todos\Infrastructure\Repositories;
+use App\Todos\Domain\Models\Todo;
 use App\Todos\Domain\Repositories\TodoRepository;
+use Illuminate\Support\Collection;
 final class EloquentTodoRepository implements TodoRepository
 {
+    public function getAllTodos(): Collection
+    {
+        return Todo::all()->reverse();
+    }
 }
