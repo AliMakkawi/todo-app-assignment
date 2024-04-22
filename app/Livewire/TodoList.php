717335ce97de808e0620e91917f0362f30c56a17
Ali Makkawi <66633670+AliMakkawi@users.noreply.github.com>
2024-04-23 00:02:34 +0300

    Allow user to view all Todos, add a new Todo, and update a Todo's task

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -3,10 +3,90 @@
 declare(strict_types=1);
 
 namespace App\Livewire;
+
+use App\Todos\Domain\Models\Todo;
+use App\Todos\Domain\Repositories\TodoRepository;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class TodoList extends Component
 {
+    public Collection $todos;
+
+    public string $task = '';
+
+    private TodoRepository $todoRepository;
+
+    public function boot(TodoRepository $todoRepository): void
+    {
+        $this->todoRepository = $todoRepository;
+    }
+
+    public function mount(): void
+    {
+        $this->getAllTodos();
+    }
+
+    private function getAllTodos(): void
+    {
+        $this->todos = $this->todoRepository->getAllTodos();
+    }
+
+    public function addTodo(): void
+    {
+        try {
+            $validatedTask = $this->validate([
+                'task' => 'required|string|max:100'
+            ]);
+
+            if ($this->todoRepository->countTodos() >= 50) {
+                session()->flash('error', 'You cannot have more than 50 to-dos.');
+                return;
+            }
+
+            $wasAdded = $this->todoRepository->addTodo($validatedTask['task']);
+
+            if ($wasAdded) {
+                $this->getAllTodos();
+                $this->task = '';
+                session()->flash('success', 'To-do successfully added.');
+            } else {
+                session()->flash('error', 'To-do failed to be added.');
+            }
+        } catch (ValidationException $exception) {
+            $errorMessage = $exception->validator->errors()->first();
+            session()->flash('error', $errorMessage);
+
+        }
+    }
+    public function updateTodoTask(Todo $todo, string $task): void
+    {
+        $validator = Validator::make(['task' => $task], [
+            'task' => 'required|string|max:100'
+        ]);
+
+        if ($validator->fails()) {
+            session()->flash('error', $validator->errors()->first());
+            return;
+        }
+
+        if($todo->task === $task) {
+            return;
+        }
+
+        $wasUpdated = $this->todoRepository->updateTodoTask($todo, $task);
+
+        if($wasUpdated) {
+            $this->getAllTodos();
+            $this->flashMessage('success', 'To-do task successfully updated.');
+            return;
+        }
+        $this->flashMessage('error', 'To-do task failed to update.');
+
+    }
     public function render(): View
     {
         return view('livewire.todo-list');
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,2 +1,84 @@
 <div class="container mx-auto">
+    <h1 class="text-center text-3xl font-bold my-4">
+        To-Do List
+    </h1>
+    <div class="w-2/3 mx-auto">
+        @if (session('success'))
+            <div class="p-4 my-4 text-md text-white rounded-lg bg-green-700" role="alert">
+                <span class="font-medium">Success!</span>
+                {{ session('success') }}
+                <button onclick="return this.parentNode.remove();" class="float-right">
+                    <x-heroicon-c-x-mark class="w-6 h-6 fill-white" />
+                </button>
+            </div>
+        @endif
+        @if (session('error'))
+            <div class="p-4 my-4 text-md text-white rounded-lg bg-red-500" role="alert">
+                <span class="font-medium">Error!</span>
+                {{ session('error') }}
+                <button onclick="return this.parentNode.remove();" class="float-right">
+                    <x-heroicon-c-x-mark class="w-6 h-6 fill-white" />
+                </button>
+            </div>
+        @endif
+    </div>
+    <div class="w-2/3 mx-auto">
+        <div class="bg-white shadow-md rounded-lg p-4">
+            <div class="mb-4 shadow-lg bg-gray-200 p-4	">
+                <form wire:submit="addTodo" class="basis-2/3 flex">
+                    <input wire:model="task" type="text" placeholder="Enter task" class="
+                    pl-2
+                    basis-10/12
+                    text-lg
+                    rounded-lg
+                    border-3
+                    border-blue">
+                    <button type="submit"  class="
+                    basis-2/12
+                    bg-blue-500
+                    hover:bg-blue-700
+                    text-white
+                    text-sm
+                    font-bold
+                    p-2
+                    ml-2
+                    rounded-full">Add To-do</button>
+                </form>
+            </div>
+            <div class="max-h-[950px] overflow-auto">
+                <table class="table-auto relative w-full">
+                    <thead class="sticky top-0 bg-gray-200">
+                    <tr>
+                        <th class="text-left p-2">Status</th>
+                        <th class="text-left p-2">Task</th>
+                        <th class="text-left p-2">Actions</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @forelse ($todos as $todo)
+                        <tr class="border">
+                            <td class="p-2">
+                            </td>
+                            <td class="p-2">
+                                <input
+                                    value="{{$todo->task}}"
+                                    wire:keydown.enter="updateTodoTask({{$todo}}, $event.target.value)"
+                                    type="text"
+                                    {{ $todo->status === \App\Todos\Domain\Enums\Status::DONE ? 'disabled' : '' }}
+                                    class="{{($todo->status === \App\Todos\Domain\Enums\Status::DONE) ? "line-through w-full" : "w-full"}}"
+                                >
+                            </td>
+                            <td class="p-2">
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="3" class="text-center text-lg font-bold">No to-dos here.</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
